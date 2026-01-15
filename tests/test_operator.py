63137e4c7bf7b6067ebd500c5019a1fdeafb34ee
emmmangcpt024 <emmmangcpt024@student.wethinkcode.co.za>
2026-01-15 08:02:58 +0200

    added tests for operators

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -1,5 +1,19 @@
 import unittest
-from operator import *
+from src.operator import *
 
 class TestOperator(unittest.TestCase):
-    pass
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(4, 3), 12)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(10, 0)
