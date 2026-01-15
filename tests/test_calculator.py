9d0d6915a3cba41f2cd902fc1c5d83e7c30dff12
emmmangcpt024 <emmmangcpt024@student.wethinkcode.co.za>
2026-01-15 08:35:28 +0200

    added tests for calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,24 @@
 import unittest
-from calculator import *
+from src.calculator import *
 
 class TestCalculate(unittest.TestCase):
-    pass
+    def test_calculate_add(self):
+        self.assertEqual(calculate(2, 3, "+"), 5)
+    
+    def test_calculate_subtract(self):
+        self.assertEqual(subtract(5, 3, "-"), 2)
+
+    def test_calculate_multiply(self):
+        self.assertEqual(multiply(4, 3, "*"), 12)
+
+    def test_calculate_divide(self):
+        self.assertEqual(divide(10, 2, "/"), 5)
+    
+    def test_calculate_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(10, 0, "/")
+
+    def test_calculate_invalid_operator(self):
+        with self.assertRaises(ValueError):
+            calculate(2, 3, "%")
+            
