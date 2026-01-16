5ca456ee2d267154b11a76391d8c45e5377d767a
emmmangcpt024 <emmmangcpt024@student.wethinkcode.co.za>
2026-01-16 10:52:45 +0200

    completed functionality of calculate by importing operator and  having every operator return the output.

--- src/calculator.py
+++ src/calculator.py
@@ -1,10 +1,20 @@
-from operator import *
+from operator import add, subtract, multiply, divide
 
-def calculate(a,b, operator):
-    pass
+def calculate(a, b, operator):
+    operations = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide
+    }
+
+    if operator not in operations:
+        raise ValueError("Invalid operator")
+
+    return operations[operator](a, b)
 
 def main():
-    pass
+    print(calculate(2, 3, "+"))
 
 if __name__ == "__main__":
     main()
